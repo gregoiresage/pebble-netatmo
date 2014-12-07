be513441d6611aac18c577a8606277114f25da05
Grégoire Sage <gregoire.sage@gmail.com>
2014-12-08 00:36:47 +0100

    update config site

--- gae/auth.py
+++ gae/auth.py
@@ -2,8 +2,17 @@ import webapp2
 from urllib import urlencode
 import json, urllib2
 
+import os
+
 from secret import app_id, app_secret
 import config
+
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 def query_json(url, data):
     """
@@ -28,9 +37,7 @@ class AuthCallback(webapp2.RequestHandler):
     def get(self):
         error = self.request.get("error")
         if error:
-            args = {}
-            args["error"] = error
-            url = "http://pebble-netatmo.appspot.com/config.html?"+urlencode(args)
+            url = "http://pebble-netatmo.appspot.com/config.html?error="+error
             self.response.location = url
             self.response.status_int = 302
             return
@@ -46,17 +53,16 @@ class AuthCallback(webapp2.RequestHandler):
         result = query_json("https://api.netatmo.net/oauth2/token", urlencode(args))
 
         if 'error' in result:
-            args = {}
-            args["error"] = error
-            url = "http://pebble-netatmo.appspot.com/config.html?"+urlencode(args)
+            url = "http://pebble-netatmo.appspot.com/config.html?error="+urlencode(result['error'])
             self.response.location = url
             self.response.status_int = 302
             return
 
         args = {}
-        args["access_token"] = result["access_token"]
+        # args["access_token"] = result["access_token"]
         args["refresh_token"] = result["refresh_token"]
-        args["expires_in"] = result["expires_in"]
+        # args["expires_in"] = result["expires_in"]
+        # url = "http://pebble-netatmo.appspot.com/config.html?"+urlencode(args)
         url = "http://pebble-netatmo.appspot.com/config.html?"+urlencode(args)
         self.response.location = url
         self.response.status_int = 302
@@ -76,8 +82,55 @@ class AuthRefreshAccessToken(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json;charset=UTF-8'
         self.response.write(json.dumps(result))
 
+class Test(webapp2.RequestHandler):
+    def get(self):
+        refresh_token = self.request.get("refresh_token")
+        error = self.request.get("error")
+        template_values = {}
+
+        if error :
+            template_values['error'] = error
+        elif refresh_token :
+            args = {}
+            args["grant_type"] = "refresh_token"
+            args["client_id"] = app_id
+            args["client_secret"] = app_secret
+            args["refresh_token"] = self.request.get("refresh_token")
+            result = query_json("https://api.netatmo.net/oauth2/token", urlencode(args))
+
+            print 'Test1 ' + json.dumps(result)
+
+            if 'error' in result :
+                template_values['error'] = result['error'];
+                print config.host + '/auth/refresh error ' + result['error'];
+            else :
+                access_token    = result['access_token']
+                refresh_token   = result['refresh_token']
+                expires_in      = result['expires_in']
+
+                args['access_token'] = access_token
+                args['app_type'] = 'app_station'
+                result = query_json("https://api.netatmo.net/api/devicelist", urlencode(args))
+
+                print 'Test2 ' + json.dumps(result)
+
+                if 'error' in result :
+                    template_values['error'] = result['error']
+                    print 'https://api.netatmo.net/api/devicelist error ' + result['error']
+                else :
+                    template_values['access_token']     = access_token
+                    template_values['refresh_token']    = refresh_token
+                    template_values['expires_in']       = expires_in
+                    template_values['devices']          = result['body']['devices']
+
+        print 'Test3 ' + json.dumps(template_values)
+
+        template = JINJA_ENVIRONMENT.get_template('templates/config.html.jinja')
+        self.response.write(template.render(template_values))
+
 application = webapp2.WSGIApplication([
     ('/auth', AuthRedirector),
     ('/auth/result', AuthCallback),
-    ('/auth/refresh', AuthRefreshAccessToken)
+    ('/auth/refresh', AuthRefreshAccessToken),
+    ('/config.html', Test)
 ], debug=True)
